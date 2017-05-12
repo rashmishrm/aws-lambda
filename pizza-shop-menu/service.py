ffb0a895b2522ff8cecb7f6c87ef2fe10f19418c
rashmishrm <rashmishrm74@gmail.com>
2017-05-12 12:45:11 -0700

    Changes

--- pizza-shop-menu/service.py
+++ pizza-shop-menu/service.py
@@ -19,49 +19,35 @@ def modifyRequest():
     tableName = "Menu"
 
 def createUpdateJSON(input_json):
-
    x = json.dumps(input_json)
    item = json.loads(x)
-   bod={}
-   ExpressionAttributeNames={}
-   ExpressionAttributeValues = {}
+   update_json={}
+   exprAttributeNames={}
+   exprAttributeValues = {}
    item = input_json
    Key = {"menu_id":item.get('menu_id')}
    UpdateExpression = 'SET '
    needComma = False
-   bod["Key"] = Key
+   update_json["Key"] = Key
    i=1;
    print(len(item))
    for k,v in item.iteritems():
        if(k!="menu_id"):
-           ExpressionAttributeNames["#"+k]=k
-           ExpressionAttributeValues[":"+k+"v"]=v
+           exprAttributeNames["#"+k]=k
+           exprAttributeValues[":"+k+"v"]=v
            UpdateExpression  = UpdateExpression + "#"+k+" = :"+k+"v"
            if(i!=len(item)):
                UpdateExpression =  UpdateExpression+","
        i=i+1;
-
-
-
-   bod["ExpressionAttributeNames"]=ExpressionAttributeNames
-   bod["ExpressionAttributeValues"]=ExpressionAttributeValues
-   bod["TableName"]="Menu"
-   #UpdateExpression = 'SET #SN = :SNV, #S = :SV, #SZ = :SZV, #P = :PV, #SH = :SHV'
-   bod["UpdateExpression"]=UpdateExpression
-   print (bod)
-   return bod
+   update_json["ExpressionAttributeNames"]=exprAttributeNames
+   update_json["ExpressionAttributeValues"]=exprAttributeValues
+   update_json["TableName"]="Menu"
+   update_json["UpdateExpression"]=UpdateExpression
+   print (update_json)
+   return update_json
 
 
 def pizzaMenuHandler(event, context):
-    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
-    access to the request and response payload, including headers and
-    status code.
-
-    To scan a DynamoDB table, make a GET request with the TableName as a
-    query string parameter. To put, update, or delete an item, make a POST,
-    PUT, or DELETE request respectively, passing in the payload to the
-    DynamoDB API as a JSON body.
-    '''
     print("Received event: " + json.dumps(event, indent=2))
     tableName ="Menu"
     operation =event["operation"]
@@ -80,9 +66,6 @@ def pizzaMenuHandler(event, context):
     if operation in operations:
         response = operations[operation](table,event.get("payload"))
         meta_data = response['ResponseMetadata']
-
-
-
 
     if operation =='GET':
         print("Response " + json.dumps(response, indent=2))
